efc2e91060383e13d8062964672ebc2203c16bbf
SaschaSaegesser <32762408+SaschaSaegesser@users.noreply.github.com>
2021-04-22 16:48:48 +0200

    Creates now a board with dynamic numbers of columns and row

---
+++ src/components/Field/Field.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./field.scss";
+
+function Field() {
+  return (
+    <li className="field" ></li>
+  );
+}
+
+export default Field;
--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,10 +1,28 @@
+import Field from "components/Field/Field";
 import React from "react";
+import uuid from "react-uuid";
 import "./board.scss";
 
 function Board() {
+  // TODO Use UI element to select columns and row? Difficulty settings?
+  const COLUMNS = 10;
+  const ROWS = 10;
+
+  const boardMatrix =
+    new Array(COLUMNS).fill(0).map(() =>
+      new Array(ROWS).fill(0));
+
   return (
-    <div className="board">
-      <h1>Board</h1>
+    <div id="board">
+      {boardMatrix.map((column) => {
+        return (
+          <ul key={uuid()}>
+            {column.map(() => {
+              return <Field key={uuid()} />;
+            })}
+          </ul>
+        );
+      })}
     </div>
   );
 }
