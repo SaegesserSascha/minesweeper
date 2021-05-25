8d997da3cf569e18f4b3b5461c3c229c415a2b00
SaschaSaegesser <32762408+SaschaSaegesser@users.noreply.github.com>
2021-05-25 14:32:46 +0200

    Refactored Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from "react";
 import "./header.scss";
+import React, { useEffect, useState } from "react";
 import { AiOutlineClockCircle } from "react-icons/ai";
 import { BsFillFlagFill } from "react-icons/bs";
 import Settings from "components/settings/Settings";
-import formatTime from "../../util/timeFormatter";
+import formatTime from "util/timeFormatter";
 
 function Header({ gameIsRunning, isVictory, updateFinalTime, settings, updateSettings, flags }) {
   const FIELDSIZE = 30; // in px
+  const ICONSIZE = 30; // in px
 
   const [clock, setClock] = useState(0);
   const width = settings.columns * FIELDSIZE;
@@ -31,25 +32,23 @@ function Header({ gameIsRunning, isVictory, updateFinalTime, settings, updateSet
       }
     }
 
-    return () => clearInterval(timer);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isVictory]);
-
-  useEffect(() => {
     if (!gameIsRunning) {
       setClock(0);
     }
+
+    return () => clearInterval(timer);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [gameIsRunning]);
 
   return (
     <div className="header" style={{ width: width }}>
       <Settings updateSettings={updateSettings} />
       <div className="undetected-mines-remaining">
-        <BsFillFlagFill size={30} />
+        <BsFillFlagFill size={ICONSIZE} />
         <h3>{settings.mines - flags}</h3>
       </div>
       <div className="timer">
-        <AiOutlineClockCircle size={30} />
+        <AiOutlineClockCircle size={ICONSIZE} />
         <h3>{formatTime(clock)}</h3>
       </div>
     </div>
